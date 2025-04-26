78aec039c079c1b85d65b3067f87861eb2eaa16e
mohamedbmoussa <bnmssamohamed@gmail.com>
2025-04-26 22:57:22 +0200

    add jwt token_required function

--- flask_auth/auth_server.py
+++ flask_auth/auth_server.py
@@ -45,6 +45,24 @@ def init_db():
 
 init_db()
 
+# JWT Token Required Decorator
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify({'error': 'Token is missing!'}), 401
+        try:
+            token = token.replace("Bearer ", "")  # Remove "Bearer " prefix if present
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            request.user = data['username']
+        except jwt.ExpiredSignatureError:
+            return jsonify({'error': 'Token expired!'}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({'error': 'Invalid token!'}), 401
+        return f(*args, **kwargs)
+    return decorated
+
 # Email Configuration
 SMTP_SERVER = os.getenv("SMTP_SERVER")
 SMTP_PORT = os.getenv("SMTP_PORT")
